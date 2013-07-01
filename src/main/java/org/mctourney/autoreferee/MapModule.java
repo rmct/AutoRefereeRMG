ca2f0c373911eb8b32ef8026aef7acf9c9865a71
Chad Waters <authorblues@gmail.com>
2013-07-01 08:41:01 -0400

    Add rudimentary module saving (no metadata)

--- src/main/java/org/mctourney/autoreferee/MapModule.java
+++ src/main/java/org/mctourney/autoreferee/MapModule.java
@@ -1,193 +1,114 @@
 package org.mctourney.autoreferee;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import com.sk89q.worldedit.CuboidClipboard;
 import com.sk89q.worldedit.data.DataException;
-
 import com.sk89q.worldedit.schematic.SchematicFormat;
-import org.bukkit.Material;
-import org.bukkit.potion.PotionEffectType;
-
-import com.google.common.collect.Sets;
 
 public class MapModule
 {
-	public enum ResourceType
+	// filenames for module elements in the zip file
+	private static final String INTERNAL_SCHM_FILENAME = "module.schematic";
+	private static final String INTERNAL_META_FILENAME = "module.xml";
+
+	// directory for packaging authored and unzipped modules temporarily
+	private static final File PACKAGING_DIRECTORY = FileUtils.getTempDirectory();
+
+	// facing yaw for "forward" down the RFW lane
+	public static final float FORWARD_YAW = 90.0f;
+
+	// range of map modules, bedrock to skybox
+	public static final int MIN_Y = 0;
+	public static final int MAX_Y = 256;
+
+	private final String slug;
+	private CuboidClipboard clipboard = null;
+
+	private MapModule(String slug)
 	{
-		/**
-		 * Materials with which to make tools and armor from.
-		 */
-		ORE(new Material[]
-		{
-			Material.IRON_ORE,
-			Material.IRON_BLOCK,
-			Material.DIAMOND_ORE,
-			Material.DIAMOND_BLOCK,
-		}),
-
-		/**
-		 * Materials with which to make tools from.
-		 */
-		WOOD(new Material[]
-		{
-			Material.WOOD,
-			Material.LOG,
-		}),
-
-		/**
-		 * Any materials that can be used to bridge quickly.
-		 */
-		BUILDING(new Material[]
-		{
-			Material.DIRT,
-			Material.WOOD,
-			Material.LOG,
-			Material.STONE,
-			Material.COBBLESTONE,
-		}),
-
-		/**
-		 * Food to restore hunger.
-		 */
-		FOOD(new Material[]
-		{
-			Material.WHEAT,
-			Material.BREAD,
-			Material.COOKIE,
-			Material.POTATO_ITEM,
-			Material.BAKED_POTATO,
-			Material.CARROT_ITEM,
-			Material.APPLE,
-			Material.RAW_CHICKEN,
-			Material.COOKED_CHICKEN,
-			Material.RAW_BEEF,
-			Material.COOKED_BEEF,
-			Material.PORK,
-			Material.GRILLED_PORK,
-			Material.RAW_FISH,
-			Material.COOKED_FISH,
-			Material.MELON,
-			Material.MELON_BLOCK,
-			Material.PUMPKIN_PIE,
-			Material.CAKE,
-			Material.MUSHROOM_SOUP,
-		}),
-
-		/**
-		 * Materials that may be used to construct a cannon.
-		 */
-		CANNON(new Material[]
-		{
-			Material.TNT,
-		}),
-
-		/**
-		 * Materials that produce light, to help with darkness.
-		 */
-		LIGHT(new Material[]
-		{
-			Material.TORCH,
-			Material.GLOWSTONE,
-		}),
-
-		/**
-		 * Water sources, which can help with lava or climbing.
-		 */
-		WATER(new Material[]
-		{
-			Material.WATER_BUCKET
-		});
-
-		Material materials[];
-		ResourceType(Material[] materials)
-		{
-			this.materials = materials;
-		}
+		this.slug = slug;
 	}
 
-	public enum HazardType
-	{
-		/**
-		 * Lava, fire, blazes, and fire zombies can all set the player on fire.
-		 */
-		FIRE(ResourceType.WATER, PotionEffectType.FIRE_RESISTANCE),
-
-		/**
-		 * Darkness restricts the players vision and often increase the danger
-		 * of other hazards within the affected area.
-		 */
-		DARKNESS(ResourceType.LIGHT, PotionEffectType.NIGHT_VISION),
-
-		/**
-		 * Areas that are filled with water may result in a player drowning.
-		 */
-		WATER(null, PotionEffectType.WATER_BREATHING),
-
-		/**
-		 * Open spaces with a clear vantage point from somewhere higher on the
-		 * opposite lane may invite arrow fire. Shared regions involve melee.
-		 */
-		PVP(null, PotionEffectType.DAMAGE_RESISTANCE),
-
-		/**
-		 * Areas forcing the player to dig can slow or stop a team.
-		 */
-		DIGGING(null, null),
-
-		/**
-		 * Areas with open gaps require bridging to cross, which is often an
-		 * excellent place for advanced PvP pressure. Building materials may
-		 * be required to progress.
-		 */
-		BRIDGING(ResourceType.BUILDING, null),
-
-		/**
-		 * Other.
-		 */
-		GENERIC(null, null);
-
-		// solutions to the hazards
-		ResourceType resource;
-		PotionEffectType effect;
-
-		HazardType(ResourceType resource, PotionEffectType effect)
-		{
-			this.resource = resource;
-			this.effect = effect;
-		}
-	}
-
-	private Set<ResourceType> resources = Sets.newHashSet();
-
-	private Set<HazardType> hazards = Sets.newHashSet();
-
-	private CuboidClipboard clipboardData = null;
-
-	private MapModule(File module, File metadata)
+	private MapModule(String slug, File module, File metadata)
 		throws IOException, DataException
 	{
-		clipboardData = SchematicFormat.MCEDIT.load(module);
+		this(slug);
+		clipboard = SchematicFormat.MCEDIT.load(module);
 		this.loadMetadata(metadata);
 	}
 
-	public static MapModule fromModuleFolder(File folder)
+	public MapModule(String slug, CuboidClipboard clipboard)
 	{
-		File module = new File(folder, "module.schematic");
-		File metadata = new File(folder, "module.xml");
+		this(slug);
+		this.clipboard = clipboard;
+	}
 
-		if (module.canRead() && !module.canRead())
-			try { return new MapModule(module, metadata); }
-			catch (IOException e) { e.printStackTrace(); }
-			catch (DataException e) { e.printStackTrace(); }
+	@Override
+	public String toString()
+	{
+		return String.format("%s[%s, w=%d, len=%d", this.getClass().getSimpleName(),
+			this.slug, this.clipboard.getWidth(), this.clipboard.getLength());
+	}
 
+	@Override
+	public int hashCode()
+	{ return this.slug.hashCode(); }
+
+	public static MapModule fromModule(File module)
+	{
 		return null;
+	}
+
+	public void saveMetadata(File metadata)
+	{
+
 	}
 
 	public void loadMetadata(File metadata)
 	{
 
 	}
+
+	private static void addToZip(ZipOutputStream zip, File f, String path, File base) throws IOException
+	{
+		zip.putNextEntry(new ZipEntry(path));
+		if (!f.isDirectory()) IOUtils.copy(new FileInputStream(f), zip);
+		else for (File c : f.listFiles()) addToZip(zip, c, base);
+	}
+
+	private static void addToZip(ZipOutputStream zip, File f, File base) throws IOException
+	{ addToZip(zip, f, base.toURI().relativize(f.toURI()).getPath(), base); }
+
+	public void save(File file) throws IOException, DataException
+	{
+		File tmp_schm = File.createTempFile("schm", null, PACKAGING_DIRECTORY);
+		File tmp_meta = File.createTempFile("meta", null, PACKAGING_DIRECTORY);
+
+		this.saveMetadata(tmp_meta);
+		SchematicFormat.MCEDIT.save(clipboard, tmp_schm);
+
+		ZipOutputStream zip = new ZipOutputStream(new
+			BufferedOutputStream(new FileOutputStream(file)));
+		zip.setMethod(ZipOutputStream.DEFLATED);
+
+		addToZip(zip, tmp_schm, INTERNAL_SCHM_FILENAME, PACKAGING_DIRECTORY);
+		addToZip(zip, tmp_meta, INTERNAL_META_FILENAME, PACKAGING_DIRECTORY);
+
+		zip.close();
+		FileUtils.deleteQuietly(tmp_schm);
+		FileUtils.deleteQuietly(tmp_meta);
+	}
+
+	public void save() throws IOException, DataException
+	{ this.save(new File(this.slug + ".arm")); }
 }
--- src/main/java/org/mctourney/autoreferee/commands/RandomizerCommands.java
+++ src/main/java/org/mctourney/autoreferee/commands/RandomizerCommands.java
@@ -1,24 +1,36 @@
 package org.mctourney.autoreferee.commands;
 
+import java.io.IOException;
+import java.util.Date;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.WorldCreator;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import org.apache.commons.cli.CommandLine;
 
 import org.mctourney.autoreferee.AutoRefMatch;
 import org.mctourney.autoreferee.AutoReferee;
+import org.mctourney.autoreferee.MapModule;
 import org.mctourney.autoreferee.RandomMatch;
 import org.mctourney.autoreferee.util.NullChunkGenerator;
 import org.mctourney.autoreferee.util.commands.AutoRefCommand;
 import org.mctourney.autoreferee.util.commands.AutoRefPermission;
 import org.mctourney.autoreferee.util.commands.CommandHandler;
 
-import java.util.Date;
+import com.sk89q.worldedit.CuboidClipboard;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.bukkit.selections.Selection;
+import com.sk89q.worldedit.data.DataException;
 
 public class RandomizerCommands implements CommandHandler
 {
+	private static final float WORLDEDIT_BASE_ROTATION = 90.0f;
+
 	AutoReferee plugin;
 
 	public RandomizerCommands(Plugin plugin)
@@ -27,7 +39,7 @@ public class RandomizerCommands implements CommandHandler
 	}
 
 	@AutoRefCommand(name={"autoref", "loadrandom"}, argmax=0, options="s+",
-		description="An example command to add.")
+		description="Generate a new random map with a given seed.")
 	@AutoRefPermission(console=true, nodes={"autoreferee.admin"})
 
 	public boolean generateRandom(CommandSender sender, AutoRefMatch match, String[] args, CommandLine options)
@@ -46,6 +58,44 @@ public class RandomizerCommands implements CommandHandler
 
 		// add the randomized map object
 		plugin.addMatch(new RandomMatch(Bukkit.createWorld(creator), seed, true));
+		return true;
+	}
+
+	@AutoRefCommand(name={"autoref", "module", "save"}, argmin=1, argmax=1, options="f",
+		description="Save a ARM to the local module directory.")
+	@AutoRefPermission(console=false, nodes={"autoreferee.configure"})
+
+	public boolean moduleSave(CommandSender sender, AutoRefMatch match, String[] args, CommandLine options)
+		throws IOException, DataException
+	{
+		Player player = (Player) sender;
+		WorldEditPlugin worldedit = AutoReferee.getWorldEdit();
+
+		Selection selection = worldedit.getSelection(player);
+		Vector vmin = selection.getNativeMinimumPoint();
+		Vector vmax = selection.getNativeMaximumPoint();
+
+		vmin.setY(MapModule.MIN_Y);
+		vmax.setY(MapModule.MAX_Y);
+
+		CuboidClipboard clipboard = new CuboidClipboard(vmax.subtract(vmin).add(1,1,1), vmin, new Vector(0,0,0));
+		clipboard.copy(worldedit.createEditSession(player));
+
+		float pfacing = Math.round(player.getLocation().getYaw() / 90.0f) * 90.0f;
+		clipboard.rotate2D((int)(WORLDEDIT_BASE_ROTATION + MapModule.FORWARD_YAW - pfacing));
+
+		int w = clipboard.getWidth();
+		if (w > 20 && !options.hasOption('f'))
+		{
+			sender.sendMessage("" + ChatColor.RED + "You are attempting to save this module with a width of " + w);
+			sender.sendMessage("" + ChatColor.RED + "If this is intentional, please add the -f flag to continue.");
+			return true;
+		}
+
+		MapModule module = new MapModule(args[0], clipboard);
+		module.save();
+
+		sender.sendMessage("" + ChatColor.GREEN + module + " saved!");
 		return true;
 	}
 }
